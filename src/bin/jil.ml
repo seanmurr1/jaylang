d098174938649b4a3d6c2ac85bab65707ac52ce3
sean-murray <sean.murray2000@hotmail.com>
2023-05-07 23:49:47 -0400

    modified driver for concolic testing

--- src/bin/jil.ml
+++ src/bin/jil.ml
@@ -5,6 +5,8 @@ let usage_msg = "jil -i <file> [<input_i>]"
 let source_file = ref ""
 let inputs = ref []
 
+let mode = ref ""
+
 let anon_fun i_raw =
   let this_i = int_of_string_opt i_raw in
   inputs := !inputs @ [ this_i ]
@@ -21,8 +23,35 @@ let run_program source =
   | Interpreter.Terminate v -> Format.printf "%a" Interpreter.pp_dvalue v
   | ex -> raise ex
 
-let () =
-  Arg.parse
-    [ ("-i", Arg.Set_string source_file, "Iutput source file") ]
-    anon_fun usage_msg ;
-  run_program !source_file
+(*****ADDITIONS BELOW******)
+
+(* Variable generator (only random integers for now) *)
+(* let var_gen = Int.quickcheck_generator *)
+let limited_var_gen = Int.gen_incl (-100) 100
+
+let test_program_concolic source = 
+  let program = Dj_common.File_utils.read_source source in 
+  let session = 
+    {
+      (Interpreter.make_default_session ()) with 
+      input_feeder = fun _ -> (Quickcheck.random_value ~seed:`Nondeterministic limited_var_gen)
+    }
+  in 
+  (* try Interpreter.test_concolically session program with *)
+  try Concolic.concolic_eval program with
+  | Interpreter.Terminate v -> Format.printf "%a" Interpreter.pp_dvalue v 
+  | Concolic.All_Branches_Hit -> Format.printf "All branches hit.\n"
+  | ex -> raise ex 
+
+let speclist = 
+  [("-i", Arg.Set_string source_file, "Input source file");
+   ("-m", Arg.Set_string mode, "Interpreter mode")]
+
+let () = 
+  Arg.parse speclist anon_fun usage_msg;
+  match !mode with 
+  | "concolic" -> test_program_concolic !source_file
+  | "normal" -> run_program !source_file
+  | _ -> ()
+
+  
